9080899c815e96cf000492f529a927058b5baa69
TarunCore <112856864+TarunCore@users.noreply.github.com>
2024-01-28 14:08:02 +0530

    same port3

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.use(cors(
       credentials: true
     }
   ))
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.use(express.json())
 app.get("/",(req, res)=>{
     res.send("Server running at"+process.env.PORT)
